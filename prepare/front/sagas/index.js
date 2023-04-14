e2dcd055eaf4818529edabe07ccf04452ea3a426
이재원 <jenyglee_30@naver.com>
2023-04-14 23:27:18 +0900

    update

---
+++ prepare/front/sagas/index.js
@@ -0,0 +1,87 @@
+import axios from "axios"
+import { all, call, fork, put, take } from "redux-saga/effects"
+
+// 통신 API 호출 함수는 generate 함수가 아니다.
+function loginAPI(data) {
+    return axios.post("/api/login", data)
+}
+
+function* login(action) {
+    // 7. 로그인할 때 데이터가 필요한데, watchLogin 함수에서 login 함수를 호출할 때 매개변수로 보내준다. { action.type : "LOG_IN_REQUEST", action.data : ID/PW }
+    // 5. 실패할 경우도 있으니 try-catch문에 넣어준다.
+    try {
+        // 3. 로그인 API 실행 함수를 호출한다.
+        const result = yield call(loginAPI, action.data) // 8. loginAPI(action.data) << 이거랑 같은 건데 형태만 다른 것이다.
+
+        // 4. 'LOG_IN_SUCCESS' 액션을 실행시킨다. 전달할 데이터도 함께 넣어준다. (dispatch와 비슷한 놈)
+        yield put({
+            type: "LOG_IN_SUCCESS",
+            data: result.data,
+        })
+    } catch (err) {
+        // 6. 실패했을 때 실행시킬 액션도 넣어준다.
+        yield put({
+            type: "LOG_IN_FAILURE",
+            data: err.response.data,
+        })
+    }
+}
+
+function logoutAPI() {
+    return axios.post("/api/logout")
+}
+
+function* logout() {
+    try {
+        const result = yield call(logoutAPI)
+
+        yield put({
+            type: "LOG_OUT_SUCCESS",
+            data: result.data,
+        })
+    } catch (err) {
+        yield put({
+            type: "LOG_OUT_FAILURE",
+            data: err.response.data,
+        })
+    }
+}
+
+function addPostAPI(data) {
+    return axios.post("/api/post", data)
+}
+
+function* addPost(action) {
+    try {
+        const result = yield call(addPostAPI, action.data)
+
+        yield put({
+            type: "ADD_POST_SUCCESS",
+            data: result.data,
+        })
+    } catch (err) {
+        yield put({
+            type: "ADD_POST_FAILURE",
+            data: err.response.data,
+        })
+    }
+}
+
+function* watchLogin() {
+    // 2-1. 'LOG_IN' action이 실행될 때까지 기다리겠다.
+    // 2-2. 'LOG_IN' action이 실행되면 login이라는 generate 함수를 호출한다.
+    yield take("LOG_IN_REQUEST", login)
+}
+
+function* watchLogout() {
+    yield take("LOG_OUT_REQUEST", logout)
+}
+
+function* watchAddPost() {
+    yield take("ADD_POST_REQUEST", addPost)
+}
+
+export default function* rootSaga() {
+    // 1. all : fork나 call로 실행하는 것들을 동시에 전부 실행할 수 있게 해준다.
+    yield all([fork(watchLogin), fork(watchLogout), fork(watchAddPost)])
+}
--- prepare/front/store/configureStore.js
+++ prepare/front/store/configureStore.js
@@ -1,8 +1,10 @@
 import { createWrapper } from "next-redux-wrapper"
 import { applyMiddleware, compose, createStore } from "redux"
 import { composeWithDevTools } from "redux-devtools-extension"
-import thunkMiddleware from "redux-thunk"
+// import thunkMiddleware from "redux-thunk"
+import createSagaMiddleware from "redux-saga"
 import reducer from "../reducers"
+import rootSaga from "../sagas"
 
 // action이 dispatcha되는 것들을 logging 하는 미들웨어
 // 미들웨어는 항상 화살표 3개를 가진다(3단 고차함수)
@@ -10,23 +12,19 @@ const loggerMiddleware =
     ({ dispatch, getState }) =>
     (next) =>
     (action) => {
-        // 이 "액션"이 실제로 함수라면 호출하고 결과를 반환합니다.
-        if (typeof action === "function") {
-            // action이 원래는 객체인데 redux-thunk에서는 action을 function으로 둘 수 있다. action이 function이면 지연함수이기 떄문에 그 액션을 나중에 실행시켜줄 수 있다.
-            return action(dispatch, getState)
-        }
-
-        // 그렇지 않으면 평소와 같이 미들웨어 체인 아래로 작업을 전달합니다.
+        console.log(action)
         return next(action)
     }
 
 const configureStore = () => {
-    const middlewares = [thunkMiddleware, loggerMiddleware]
+    const sagaMiddleware = createSagaMiddleware()
+    const middlewares = [sagaMiddleware, loggerMiddleware]
     const enhancer =
         process.env.NODE_ENV === "production"
             ? compose(applyMiddleware(...middlewares))
             : composeWithDevTools(applyMiddleware(...middlewares)) // 개발자모드에서 DevTools 사용 세팅한다.
     const store = createStore(reducer, enhancer)
+    store.sagaTask = sagaMiddleware.run(rootSaga)
     return store
 }
 const wrapper = createWrapper(configureStore, {
